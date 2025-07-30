7f12880a292d43f38f2d15230197f2d62e5cd287
jeanne <kk.ap.2546@gmail.com>
2025-07-30 19:12:50 +0700

    Add built transaction page

---
+++ frontend/src/components/transaction/History.tsx
@@ -0,0 +1,111 @@
+const History = () => {
+    return (
+        <div className="bg-white p-5 rounded-3xl">
+            <div className="flex flex-col gap-y-5 mt-5">
+                <div className="flex flex-col px-2 md:px-5">
+                    <span className="text-[#666] font-semibold">Today</span>
+                    <div className="flex flex-col px-2 md:px-5">
+                        <div className="border-b border-gray-200 py-3">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col ">
+                                    <span className="font-bold">Salary Payment</span>
+                                    <span className="text-[#666]">2:30 PM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#4ecdc4] text-lg">+฿15,000.00</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="border-b border-gray-200 py-3 ">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Shopping</span>
+                                    <span className="text-[#666]">1:24 PM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#ff6b6b] text-lg">-฿12,500.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col px-2 md:px-5">
+                    <span className="text-[#666] font-semibold">Yesterday</span>
+                    <div className="flex flex-col  px-2 md:px-5">
+                        <div className="border-b border-gray-200 py-3 ">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Electricity Bill</span>
+                                    <span className="text-[#666]">10:15 AM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#ff6b6b] text-lg">-฿2,150.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col px-2 md:px-5">
+                    <span className="text-[#666] font-semibold">4 July 2025</span>
+                    <div className="flex flex-col  px-2 md:px-5">
+                        <div className="border-b border-gray-200 py-3 ">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Netflix Subscription</span>
+                                    <span className="text-[#666]">8:15 AM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#ff6b6b] text-lg">-฿350.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col  px-2 md:px-5">
+                        <div className="border-b border-gray-200 py-3 ">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Netflix Subscription</span>
+                                    <span className="text-[#666]">8:15 AM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#ff6b6b] text-lg">-฿350.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col  px-2 md:px-5">
+                        <div className="border-b border-gray-200 py-3 ">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Netflix Subscription</span>
+                                    <span className="text-[#666]">8:15 AM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#ff6b6b] text-lg">-฿350.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col  px-2 md:px-5">
+                        <div className="border-b border-gray-200 py-3 ">
+                            <div className="flex justify-between items-center hover:translate-x-1 duration-300">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Netflix Subscription</span>
+                                    <span className="text-[#666]">8:15 AM</span>
+                                </div>
+                                <div className="">
+                                    <span className="font-semibold text-[#ff6b6b] text-lg">-฿350.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default History
--- frontend/src/pages/Transaction.tsx
+++ frontend/src/pages/Transaction.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
+import Navbar from "../components/Navbar"
+import History from "../components/transaction/History";
+
+
 const Transactions = () => {
+    const [filter, setFilter] = useState<number>(0);
+
+    function handleFilter(num: number) {
+        setFilter(num);
+    }
+
     return (
-        <div>Transaction</div>
+        <div className="bg-gradient-to-br from-indigo-400 to-purple-500 min-h-screen">
+            <div className="flex flex-col px-3 py-3 md:px-20 md:py-3 gap-y-5 md:gap-y-10 text-sm md:text-base">
+                <div className=""><Navbar /></div>
+                <div>
+                    <div className="flex gap-x-3">
+                        <button onClick={() => handleFilter(0)}
+                            className={`py-1 px-5 rounded-4xl border-1 font-semibold text-gray-400 cursor-pointer ${filter === 0 ? "bg-[#6C47FF] text-white border-[#6C47FF]" : "bg-white border-gray-400"}`}
+                        >
+                            All
+                        </button>
+                        <button onClick={() => handleFilter(1)}
+                            className={`py-1 px-5 rounded-4xl border-1 font-semibold text-gray-400 cursor-pointer ${filter === 1 ? "bg-[#6C47FF] text-white border-[#6C47FF]" : "bg-white border-gray-400"}`}
+                        >Transfer/Receive
+                        </button>
+                    </div>
+                    <div className="mt-2"><History /></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
